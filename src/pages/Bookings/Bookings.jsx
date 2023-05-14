117024cf85266be75aa91a6320854644cc690a24
Aminul667 <rahathssn@gmail.com>
2023-05-14 17:38:49 +0600

    70-8 Create Delete API and Delete a service Booking client

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -15,6 +15,24 @@ const Bookings = () => {
       .then((data) => setBookings(data));
   }, []);
 
+  const handleDelete = (id) => {
+    const proceed = confirm("Are you sure you want to delete");
+    if (proceed) {
+      fetch(`http://localhost:5000/bookings/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            alert("Deleted Successfully!");
+            const remaining = bookings.filter(booking => booking._id !== id);
+            setBookings(remaining);
+          }
+        });
+    }
+  };
+
   return (
     <div>
       <h2>Bookings: {bookings.length}</h2>
@@ -28,19 +46,21 @@ const Bookings = () => {
                   <input type="checkbox" className="checkbox" />
                 </label>
               </th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
+              <th>Image</th>
+              <th>Service</th>
+              <th>Date</th>
+              <th>Price</th>
+              <th>Status</th>
             </tr>
           </thead>
           <tbody>
-            {
-                bookings.map(booking => <BookingRow
-                    key={booking._id}
-                    booking={booking}
-                ></BookingRow>)
-            }
+            {bookings.map((booking) => (
+              <BookingRow 
+              key={booking._id} 
+              booking={booking}
+              handleDelete={handleDelete}
+              ></BookingRow>
+            ))}
           </tbody>
         </table>
       </div>
