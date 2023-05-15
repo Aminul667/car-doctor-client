4317634c3f96aed38b1ae5d2bceebe6075dab4d5
Aminul667 <rahathssn@gmail.com>
2023-05-15 17:43:25 +0600

    71-8 Deploy to vercel, firebase and push updates to client side

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -10,7 +10,7 @@ const Bookings = () => {
 
   console.log(user);
 
-  const url = `http://localhost:5000/bookings?email=${user?.email}`;
+  const url = `https://car-doctor-server-six-ruddy.vercel.app/bookings?email=${user?.email}`;
 
   useEffect(() => {
     fetch(url, {
@@ -32,7 +32,7 @@ const Bookings = () => {
   const handleDelete = (id) => {
     const proceed = confirm("Are you sure you want to delete");
     if (proceed) {
-      fetch(`http://localhost:5000/bookings/${id}`, {
+      fetch(`https://car-doctor-server-six-ruddy.vercel.app/bookings/${id}`, {
         method: "DELETE",
       })
         .then((res) => res.json())
@@ -48,7 +48,7 @@ const Bookings = () => {
   };
 
   const handleBookingConfirm = (id) => {
-    fetch(`http://localhost:5000/bookings/${id}`, {
+    fetch(`https://car-doctor-server-six-ruddy.vercel.app/bookings/${id}`, {
       method: "PATCH",
       headers: {
         "content-type": "application/json",
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -47,7 +47,7 @@ const AuthProvider = ({ children }) => {
       if (currentUser && currentUser.email) {
         const loggedUser = { email: currentUser.email };
 
-        fetch("http://localhost:5000/jwt", {
+        fetch("https://car-doctor-server-six-ruddy.vercel.app/jwt", {
           method: "POST",
           headers: {
             "content-type": "application/json",
