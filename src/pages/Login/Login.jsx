e779e832e085cce4a0b5b591b0e1c6e095341f97
Aminul667 <rahathssn@gmail.com>
2023-05-15 11:20:36 +0600

    71-4 Create JWT Token on server, Get jwt token on client side client

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import img from "../../assets/images/login/login.svg";
 import { useContext } from "react";
 import { authContext } from "../../providers/AuthProvider";
@@ -6,20 +6,36 @@ import { authContext } from "../../providers/AuthProvider";
 const Login = () => {
   const { signIn } = useContext(authContext);
   const location = useLocation();
+  const navigate = useNavigate();
 
-  const from = location.state?.from?.pathname || '/';
+  const from = location.state?.from?.pathname || "/";
 
   const handleLogin = (event) => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(email, password);
 
     signIn(email, password)
       .then((result) => {
         const user = result.user;
-        console.log(user);
+        const loggedUser = { email: user.email };
+        console.log(loggedUser);
+        
+        fetch("http://localhost:5000/jwt", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(loggedUser),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log('jwt response', data);
+            // set token to local storage(second best place)
+            localStorage.setItem('car-access-token', data.token);
+            navigate(from, { replace: true });
+          });
       })
       .then((error) => {
         console.log(error);
@@ -52,7 +68,7 @@ const Login = () => {
                   <span className="label-text">Password</span>
                 </label>
                 <input
-                  type="text"
+                  type="password"
                   name="password"
                   placeholder="password"
                   className="input input-bordered"
